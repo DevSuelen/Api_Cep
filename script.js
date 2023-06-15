860366a9ccac3d6c98d02f80e768523a7d0e68f5
DannyelHolanda <dannyelholanda@gmail.com>
2023-06-15 20:39:43 -0300

    api de cep

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-const findEstados = () =>{
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-      .then(response=> response.json())
-      .then(json => {
+const findEstados = () => {
+  fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    .then(response => response.json())
+    .then(json => {
 
-        let inputEstado = estado
+      let estados = ''
 
-        let estados = ''
-
-        json.forEach(estado => {
-            estados += `<option value = ${estado.sigla}>${estado.nome}</option>`
-            
-        });
-      })console.log(estado)
-     }
+      json.forEach(estado => estados += `<option value = ${estado.sigla}>${estado.nome}</option>`);
+      uf.innerHTML = estados
+    })
+}
 
 findEstados()
 
 
+
+const findCep = () => {
+
+  let cep_ = cep.value
+  console.log(cep_)
+
+  fetch(`https://viacep.com.br/ws/${cep_}/json/`)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json)
+        prencherCampos(json)
+        
+
+    })
+}
+
+
+
+const prencherCampos = (json) => {
+
+
+  logradouro.value = json.logradouro
+  numero.focus()
+  bairro.value = json.bairro
+  complemento.value = json.complemento
+  uf.value= json.uf
+  localidade.value = json. localidade
+  
+ 
+}
+ 
+
+
+findCep()
